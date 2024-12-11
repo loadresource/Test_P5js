ab659067f081d53a8d731cbaa309e82941678569
kelvin <63316084+loadresource@users.noreply.github.com>
2024-12-11 14:44:51 -0400

    first commit to test

---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+}
+
+function draw() {
+  fill(0);
+  stroke(255);
+  strokeWeight(2);
+  if(mouseIsPressed == true){
+    circle(mouseX,mouseY,50)
+  }
+}
