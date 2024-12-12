e61d5edbab646c8965f351d11c9ca05bf402206c
kelvin <63316084+loadresource@users.noreply.github.com>
2024-12-11 23:42:59 -0400

    Update sketch.js
    mouseOver effect

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -7,7 +7,13 @@ function draw() {
   fill(0);
   stroke(255);
   strokeWeight(2);
-  if(mouseIsPressed == true){
-    circle(mouseX,mouseY,random(10,50))
+  if(mouseIsPressed) {
+    circle(mouseX, mouseY, random(25,50));
   }
+  // When mouse is just hovering - create smaller, unfilled circles
+  else {
+    fill(0);
+    stroke(color(random(0,255), 0, random(0,255))); // Purple-ish colors
+    circle(mouseX, mouseY, random(10,20));
+  }  
 }
